439a18cc772664ca0abeb92bb97f1511a3d3a272
Oliver Zhang <oli.zhang@gmail.com>
2016-06-15 12:08:54 +0800

    time stamp for new stories

--- app/scripts/ad.js
+++ app/scripts/ad.js
@@ -305,7 +305,7 @@ function writeAd(adType, returnSrc) {
   currentAdCount = adCount[adType];
   if (currentAdCount < adMax[adType]) {
     adPosition = adPositions[adType][currentAdCount];
-    iframeSrc = '/m/marketing/'+adFileName+'.html?v=4' + bannerBG + '#adid='+ adch + adPosition + '&pid='+adType+adCount[adType];
+    iframeSrc = '/m/marketing/'+adFileName+'.html?v=5' + bannerBG + '#adid='+ adch + adPosition + '&pid='+adType+adCount[adType];
     if (/mpu/.test(adType)) {
       adWidth = '300';
       adHeight = '250';
