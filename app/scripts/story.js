9650d89a81911c89983ef1ea03df74bc0ebaa3e8
wanbo <gewanbo@gmail.com>
2016-07-14 17:37:01 +0800

    Switch to 61009 instead of 61008

--- app/scripts/story.js
+++ app/scripts/story.js
@@ -8,7 +8,7 @@ var recommendLoaded = false;
 var recommendInner = document.getElementById('story-recommend');
 
 message.head = {};
-message.head.transactiontype = '61008';
+message.head.transactiontype = '61009';
 message.head.source = 'web';
 message.body = {};
 message.body.ielement = {};
@@ -71,11 +71,11 @@ xhr.onload = function() {
         //alert('Request failed.  Returned status of ' + xhr.status);
     }
 
-    if(!recommendLoaded && message.head.transactiontype === '61008'){
-        message.head.transactiontype = '61009';
-        xhr.open(ajaxMethod, encodeURI(ajaxUrl));
-        xhr.send(JSON.stringify(message));
-    }
+    // if(!recommendLoaded && message.head.transactiontype === '61008'){
+    //     message.head.transactiontype = '61009';
+    //     xhr.open(ajaxMethod, encodeURI(ajaxUrl));
+    //     xhr.send(JSON.stringify(message));
+    // }
 };
 xhr.send(JSON.stringify(message));
 
