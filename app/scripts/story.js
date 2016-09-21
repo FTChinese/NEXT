4b6be585da95efa1761d15f7b3a23cb4fe8d2129
Oliver Zhang <oli.zhang@gmail.com>
2016-09-21 08:54:42 +0800

    change story.js third party number from 20 to 8

--- app/scripts/story.js
+++ app/scripts/story.js
@@ -8,7 +8,7 @@ var message = {};
 var recommendLoaded = false;
 var recommendInner = document.getElementById('story-recommend');
 var recommendVersion;
-var thirdPartAPIUrl = 'http://120.27.47.77:8091/getRtCmd?siteId=5002&num=20&itemId=' + FTStoryid;
+var thirdPartAPIUrl = 'http://120.27.47.77:8091/getRtCmd?siteId=5002&num=8&itemId=' + FTStoryid;
 var thirdPartFeedbackUrl = 'http://120.27.47.77:8091/rec/click?siteId=5002&itemId=' + FTStoryid;
 var thirdPartData = [];
 var userId;
