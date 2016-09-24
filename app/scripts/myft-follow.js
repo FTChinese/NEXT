233d8892f6c59e518c3d0504e253bead311fb340
Oliver Zhang <oli.zhang@gmail.com>
2016-09-24 10:15:48 +0800

    fix bug in myft-follow.js contains

--- app/scripts/myft-follow.js
+++ app/scripts/myft-follow.js
@@ -92,16 +92,18 @@ try {
 }
 
 function contains(a, obj) {
-    for (var i = 0; i < a.length; i++) {
-        if (a[i].value === obj.value && a[i].tag === obj.tag) {
-            // console.log ('yes:');
-            // console.log (a[i]);
-            // console.log (obj);
-            return true;
-        } else {
-            // console.log ('no:');
-            // console.log (a[i]);
-            // console.log (obj);
+    if (a && a.length>0 && obj && obj.value && obj.tag) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i].value === obj.value && a[i].tag === obj.tag) {
+                // console.log ('yes:');
+                // console.log (a[i]);
+                // console.log (obj);
+                return true;
+            } else {
+                // console.log ('no:');
+                // console.log (a[i]);
+                // console.log (obj);
+            }
         }
     }
     return false;
