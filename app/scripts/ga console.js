6c41145eddb928c82c610ea05168b16e43d324af
Oliver Zhang <[oli.zhang@gmail.com]>
2017-05-08 17:14:30 +0800

    track dc ad performance

--- app/scripts/ga console.js
+++ app/scripts/ga console.js
@@ -5,6 +5,7 @@ var tableRows = document.getElementById('ID-eventPanel-Table').querySelectorAll(
 var request = 0;
 var success = 0;
 var successOnRetry = 0;
+var failOnRetry = 0;
 var fail = 0;
 
 for (var i = 0; i<tableRows.length; i++) {
@@ -20,12 +21,18 @@ for (var i = 0; i<tableRows.length; i++) {
 		successOnRetry += dataValue;
 	} else if (dataName === 'Fail') {
 		fail += dataValue;
+	} else if (dataName === 'Fail on Retry') {
+		failOnRetry += dataValue;
 	}
 }
 
 var rate = 100 * (success + successOnRetry) / request;
 rate = (Math.round(rate * 10))/10;
 rate = 'Overall Success rate is ' + rate + '%';
+
+var rateUplimit = 100 * (success + successOnRetry + failOnRetry) / request;
+rateUplimit = (Math.round(rateUplimit * 10))/10;
+rateUplimit = 'Overall Success rate cannot be more than ' + rateUplimit + '% no matter how many times you try. ';
 
 var failRate = 100 * fail / request;
 failRate = (Math.round(failRate * 10))/10;
@@ -36,9 +43,10 @@ firstSuccessRate = (Math.round(firstSuccessRate * 10))/10;
 firstSuccessRate = 'First Request Success rate is ' + firstSuccessRate + '%';
 
 console.clear();
-console.log (request + ' requests, ' + success + ' succeed at once, ' + successOnRetry + ' succeed on retry. ');
+// console.log (request + ' requests, ' + success + ' succeed at once, ' + successOnRetry + ' succeed on retry. ');
 console.log (firstSuccessRate);
 console.log (failRate);
+console.log (rateUplimit);
 console.log (rate);
 }
 
