eff09f0e5d700ecbeabeb23cf1209a0e824ed38b
Oliver Zhang <[oli.zhang@gmail.com]>
2017-09-19 10:58:41 +0800

    check font size

--- app/scripts/story.js
+++ app/scripts/story.js
@@ -171,9 +171,9 @@ function changeFontSize() {
 
 
 
-function checkFontSize() {
+function checkFontSize(forceFontSize) {
     /* jshint ignore:start */
-    fs = GetCookie('fs');
+    fs = forceFontSize || GetCookie('fs');
     /* jshint ignore:end */
     if (typeof fs === 'string' && fs !== null && fs !== '' && document.getElementById('font-options') && document.querySelector('.story-container')) {
         document.getElementById('font-options').querySelector('.' + fs.replace(/ /g, '.')).className = fs + ' selected';
