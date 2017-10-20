ea00ee11c5e5c26931cb6ad8875ae8ffcd96325c
wangyichen1064431086 <1064431086@qq.com>
2017-10-20 11:21:00 +0800

    Modify adPosition to position

--- app/scripts/ad.js
+++ app/scripts/ad.js
@@ -532,7 +532,7 @@ function writeAdNew(obj) {
    * @param obj 
    * @param obj.devices: TYPE Array, the devices are allowed to show this ad, Eg:['PC','PadWeb','iPhoneWeb','AndroidWeb']
    * @param obj.pattern: TYPE String,the key string of var adPattern, Eg：'FullScreen'、'Leaderboard'
-   * @param obj.adPosition：TYPE String, the key string of var adPattern.xxx.position,Eg: 'Num1','Right1','Middle2'
+   * @param obj.position：TYPE String, the key string of var adPattern.xxx.position,Eg: 'Num1','Right1','Middle2'
    * @param obj.container: TYPE String, the container specified for the ad position in a certain page. The priority of obj.container is the highest among obj.container,adPattern.container and 'none'
    */
   //MARK: First, get the adid
