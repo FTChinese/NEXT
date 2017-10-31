d55b4e00ec2f467c2f6e1ff970540d3b03b88c63
Oliver Zhang <[oli.zhang@gmail.com]>
2017-10-31 11:54:51 +0800

    ad change

--- app/scripts/ad-third-party.js
+++ app/scripts/ad-third-party.js
@@ -1,4 +1,4 @@
-/* exported adReachability,showTextImageForAd, gCanReplaceInstoryWithAd, gReplacedInstroyWithAd */
+/* exported adReachability,showTextImageForAd, gCanReplaceInstoryWithAd, gReplacedInstroyWithAd, getRandomInt */
 var gCanReplaceInstoryWithAd = false;//表征文章内推荐块内容是否准备好，如果准备好，则可以将其用Ad替换
 var gReplacedInstroyWithAd = false;//表征文章内推荐块内容已经用Ad替换
 
