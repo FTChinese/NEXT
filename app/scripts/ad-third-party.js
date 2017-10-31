a8bcb4e3cceef66f6728a88de78ec0fc3c165b82
wangyichen1064431086 <1064431086@qq.com>
2017-10-31 18:41:09 +0800

    Some work for hennessy

--- app/scripts/ad-third-party.js
+++ app/scripts/ad-third-party.js
@@ -74,23 +74,25 @@ function adReachability() {
 
 
 function showTextImageForAd(sourceInfo) {
-  //console.log ('show text image for ad!')
+  
   if (gCanReplaceInstoryWithAd === true && gReplacedInstroyWithAd === false) {
-   //正式模式   
+   //正式模式
+    console.log ('show text image for ad!');   
     var adLink = decodeURIComponent(sourceInfo.adLink)||'https://www.hennessy.com/zh-cn/hennessyparadisimperial/campaign.htm?utm_source=Ftchinese%20homepage%20top%20banner&utm_campaign=HPI2017OcttoNovCampaignDigital&utm_content=Ad&utm_term=OcttoNovCampaign&smtid=509415688z2906z1cm6gzacz0z';
     var adTitle = sourceInfo.adTitle || '轩尼诗顶通20171031';
     // MARK: open the resource in an iFrame
     var randomNumber = Math.random();
     //console.log ('random number is ' + randomNumber);
-    if (randomNumber < 0 && window.footerMoreShowed === undefined) {
+    if (randomNumber < 0.09 && window.footerMoreShowed === undefined) {
       var w2 = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
       var h2 = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
       var footerEle = document.querySelector('.footer-container');
       footerEle.innerHTML += '<div id="footer-more"></div>';
       var footerMore = document.getElementById('footer-more');
-      footerMore.innerHTML = '<div class="hide"><iframe width="' + w2 + '" height="'+ h2 +'" frameborder="0" scrolling="no" marginwidth="0" marginheight="0" style="" src="' + adLink + '"></iframe></div>';
+      footerMore.innerHTML = '<div><iframe width="' + w2 + '" height="'+ h2 +'" frameborder="0" scrolling="no" marginwidth="0" marginheight="0" style="" src="' + adLink + '"></iframe></div>';
       ga('send','event','Text and Image Ad', 'click footer', adTitle);
       window.footerMoreShowed = true;
+      gReplacedInstroyWithAd = true;
     }
   }
 }
