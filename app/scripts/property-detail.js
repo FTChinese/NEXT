a5016ae0e215baf2bc616d62ac324f2348128a97
dreamernumber1 <hhy1770547592@163.com>
2017-12-06 13:28:37 +0800

    add check isExist username

--- app/scripts/property-detail.js
+++ app/scripts/property-detail.js
@@ -12,19 +12,51 @@
          }
 
     });
-
+    // var isLegalUser = true;
     $('body').on('blur', '#client-email', function () {
         checkUserName();
+        // var userName = $('#client-email').val();
+        // var isTrue = isEmail(userName)
+        // if (userName != ''){
+        //      $('#user-hint').css('display', 'block');
+        //      if (isTrue === true){
+        //         $('#client-email').css('background','#fff');
+        //     }
+        //     if (isTrue === false){
+        //         $('#client-email').val('');//清空内容 
+        //         $('#client-email').attr('placeholder','请输入正确格式邮箱！');
+        //         $('#client-email').css('background','red');
+        //         isLegalUser = false;
+        //         return;
+        //     }
+        //     $.ajax({
+        //         type: 'get',
+        //         url: '/index.php/property/inquiry',
+        //         success: function(data) {
+        //             if (data === 'existence') {
+        //                 $('#user-hint').html(''+userName+' 用户名已被注册');
+        //                 isLegalUser = false;
+        //                 return;
+        //             }else{
+        //                 $('#user-hint').html('用户名可用');
+        //                 isLegalUser = true;
+        //             }
+        //         },
+        //         error: function() {
+        //             $('#user-hint').html('服务器未能正确响应');
+        //             isLegalUser = false;
+        //             return;
+        //         }
+        //     });
+        // }
     });
-    // $('body').on('blur', '#client-number', function () {
-    //     checkTel();
-    // });
+
  
     function isEmail(str){ 
         var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/; 
         return reg.test(str); 
     }
-    // var isTrue = false
+
     function checkUserName(){
         var userName = $('#client-email').val();
         var isTrue = isEmail(userName);
@@ -58,6 +90,7 @@
             // alert('请您输入您的电子邮箱或者联系电话');
             if ((email === '')&&(number === '')){
                 $('#user-hint').css('display', 'block');
+                $('#user-hint').html('请您输入您的电子邮箱或者联系电话?');
                 $('#client-name').css('background','#fff');
                 $('#client-email').css('background','#fff');
                 $('#client-email').attr('placeholder','Email');
@@ -79,8 +112,8 @@
             $(this).html('正在提交...');
             $.ajax({
                 type: 'post',
-                url: '/api/inquiry/post',
-                // dataType: 'text',
+                url: '/index.php/property/inquiry',
+                dataType: 'json',
                 data: {
                        name:name,
                        number:number,
@@ -89,11 +122,17 @@
                        propertyId:propertyId
                    },
                 success: function(data) {
-                    console.log('欢迎您！'+data);
-                    $(this).html('欢迎您！'+name).unbind();
+                    if (data === 'existence') {
+                        $(this).html('账号已存在，请重新输入！');
+                        return;
+                    }else{
+                        console.log('欢迎您！'+data);
+                        $(this).html('欢迎您！'+name).unbind();
+                    }
+                    
                 },
                 error: function() {
-                     $(this).html('注册失败，请再次提交！');
+                     $(this).html('联系失败，请再次提交！');
                     return;
                 }
             });
@@ -104,23 +143,34 @@
             
         }
         
-    // function checkTel() {
-    //   var obj = document.getElementById('client-number');
-    //   var value = obj.value;
-    //   var regTel2 = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value);
-    //   if (value !== '') { 
-    //     if (!regTel2) { 
-    //     //   alert('电话号码输入有误！');
-    //       $('#client-number').val('');//清空内容 
-    //       $('#client-number').attr('placeholder','请输入正确电话号码！');
-    //       $('#client-number').css('background','#FFEC1A');
-    //       return ;
+    // var isExistNumber = true;
+    // $('body').on('blur', '#client-number', function () {
+    //     var number = $('#client-number').val();
+    //     if (number != ''){
+    //         $.ajax({
+    //             type: 'get',
+    //             url: '/index.php/property/inquiry',
+    //             success: function(data) {
+    //                 if (data === 'existence') {
+    //                     $('#user-hint').html(''+number+' 电话账号已被注册');
+    //                     isExistNumber = false;
+    //                     return;
+    //                 }else{
+    //                     $('#user-hint').html('用户名可用');
+    //                     isExistNumber = true;
+    //                 }
+    //             },
+    //             error: function() {
+    //                 $('#user-hint').html('服务器未能正确响应');
+    //                 isExistNumber = false;
+    //                 return;
+    //             }
+    //         });
     //     }
-    //   }
-    // //   alert('电话号码输入正确！');
-    //   $('#client-number').css('background','#fff');
-    //   return true;
-    // }
+    // });
+
+
+
     });
    
 })(); 
