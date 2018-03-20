7cfaf0500a95b8bab05d69e9cbb3205da5f60d40
Oliver Zhang <[oli.zhang@gmail.com]>
2018-03-20 10:06:18 +0800

    move guid() to key.js

--- app/scripts/key.js
+++ app/scripts/key.js
@@ -53,6 +53,16 @@ function paravalue(theurl, thep) {
     return thev;
 }
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
 function trackerNew() {
     var l=window.location.href;
     var keyTag; 
--- app/scripts/story.js
+++ app/scripts/story.js
@@ -454,15 +454,7 @@ function bindFeedbackEvent(){
 }
 
 
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-}
+
 
 // MARK:Getting a random integer between two values.The maximum is exclusive and the minimum is inclusive
 
