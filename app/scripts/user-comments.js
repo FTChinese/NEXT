f9bd2e9111a5b0b146477f0441e75cab2b81c2dc
Oliver Zhang <[oli.zhang@gmail.com]>
2018-04-21 13:31:23 +0800

    pass login to native

--- app/scripts/user-comments.js
+++ app/scripts/user-comments.js
@@ -473,14 +473,30 @@ function presentAlert(title, message) {
     }
 }
 
-function passLoginToNative(){
-    var userInfo = {
-        username: username || '',
-        userId: window.userId || ''
-    };
-    try {
-        webkit.messageHandlers.user.postMessage(userInfo);
-    } catch(ignore) {
 
+function passLoginToNative() {
+    var message = {};
+    var uniqueId = GetCookie('uniqueVisitorId') || guid();
+    window.username = GetCookie('USER_NAME') || '';
+    if (window.username !== '') {
+        message = {
+            'username': username,
+            'userId': userId,
+            'uniqueVisitorId': uniqueId
+        };
+        // MARK: Get subscription: standard/premium
+        var paywall = GetCookie('paywall') || '';
+        var paywallExpire = GetCookie('paywall_expire') || '';
+        if (paywall !== '') {
+            message.paywall = paywall;
+        }
+        if (paywallExpire !== '') {
+            message.paywallExpire = paywallExpire;
+        }
+    } else {
+        message = {
+            'uniqueVisitorId': uniqueId
+        }
     }
+    webkit.messageHandlers.user.postMessage(message);
 }
