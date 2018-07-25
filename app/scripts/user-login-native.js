cff75025fed37aad83d6016e786b4497f7440cf7
Oliver Zhang <[oli.zhang@gmail.com]>
2018-07-25 13:12:23 +0800

    fix bug with paywall

--- app/scripts/user-login-native.js
+++ app/scripts/user-login-native.js
@@ -1,5 +1,6 @@
 // FIXME: There's an exact same function in the NEXT project. It needs to be handled. 
 function passLoginToNative() {
+    // MARK: Use info from cookie for speed
     var message = {};
     var uniqueId = GetCookie('uniqueVisitorId') || guid();
     var userNameForLogin = GetCookie('USER_NAME') || GetCookie('USER_NAME_FT') || '';
@@ -23,6 +24,33 @@ function passLoginToNative() {
     } catch (ignore) {
         
     }
+    // MARK: Valid the info with a network request
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/index.php/jsapi/paywall');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var userInfo = JSON.parse(xhr.responseText);
+            if (userInfo.paywall === 0) {
+                if (userInfo.premium === 1) {
+                    message.paywall = 'premium';
+                } else {
+                    htmlClass += ' is-subscriber is-standard';
+                    message.paywall = 'standard';
+                }
+                message.paywallExpire = userInfo.paywallExpire || '';
+            } else {
+                message.paywall = '';
+                message.paywallExpire = '';
+            }
+            try {
+               webkit.messageHandlers.user.postMessage(message);
+            } catch (ignore) {
+                
+            }
+        }
+    };
+    xhr.send();
 }
 
 // MARK: This will throw error if not in iOS WKWebview
