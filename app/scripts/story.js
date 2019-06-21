0e6de33c283d74d5f7b3e07b816a75ed5ba0f310
Oliver Zhang <[oli.zhang@gmail.com]>
2019-06-21 11:22:49 +0800

    track copy

--- app/scripts/story.js
+++ app/scripts/story.js
@@ -552,5 +552,14 @@ try {
 
 }
 
-
+// MARK: - When the user copied our story text, add some copyright info at the top of clipboardData
+document.addEventListener('copy', function(e) {
+    var text = window.getSelection().toString();
+    var copyRight = '请使用文章顶部或底部的共享按钮来进行共享文章的链接。未经允许复制文章内容分享给他人是违反FT中文网条款和条件以及版权政策的行为。发送电子邮件至customer.service@ftchinese.com以购买其他权利。有关更多信息，请访问http://www.ftchinese.com/m/corp/copyright.html。\r\n\r\n' + location.href + '\r\n\r\n';
+    e.clipboardData.setData('text/plain', copyRight + text);
+    var copyTextLength = text.length;
+    ga('set', 'metric2', copyTextLength);
+    ga('send','event','Copy Story', 'Copy', ftItemId, {'nonInteraction':1});
+    e.preventDefault();
+});
 
