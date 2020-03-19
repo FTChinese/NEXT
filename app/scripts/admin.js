64b60899801e919a515b1a420f0dd4152328d895
olizh <[oli.zhang@gmail.com]>
2020-03-19 09:02:49 +0800

    format

--- app/scripts/admin.js
+++ app/scripts/admin.js
@@ -30,6 +30,7 @@
         xhr.onload = function() {
             if (xhr.status === 200 && xhr.responseText) {
                 console.log ('removed!');
+                updateUserInfo();
             } else  {
                 alert('Something is wrong. Please try later! ');
             }
@@ -73,6 +74,7 @@
         xhr.onload = function () {
             alert ('Your data is successfully submited. Now let us verify the response! ');
             console.log(xhr.responseText);
+            updateUserInfo();
         };
         xhr.onerror = function() {
             alert('There is an error on the server side. Please try later! ');
@@ -81,5 +83,42 @@
         xhr.send(fd);
         return false;
     });
+
+    function updateUserInfo() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', window.userInfo);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                console.log(data);
+                var rows = '';
+                var keys = Object.keys(data);
+                for (var i=0; i < keys.length; i++) {
+                    var key = keys[i];
+                    rows += '<tr data-user-id="' + key + '"><td class="o-table__cell--numeric"><input type="checkbox"></td><td class="o-table__cell--numeric">' + data[key].email + '</td><td class="o-table__cell--numeric">' + data[key].user_name + '</td><td class="o-table__cell--numeric"><a class="remove-user">Remove</a></td></tr>';
+                }
+                var table = '';
+                table += '<table class="o-table o-table--horizontal-lines o-table--responsive-overflow" data-o-component="o-table" data-o-table-responsive="overflow">';
+                table += '<thead>';
+                table += '    <tr>';
+                table += '        <th data-column-default-sort="ascending" scope="col" role="columnheader" data-o-table-data-type="number" class="o-forms-input--checkbox"><input type="checkbox"></th>';
+                table += '        <th data-column-default-sort="ascending" scope="col" role="columnheader" data-o-table-data-type="number">Email</th>';
+                table += '        <th data-column-width scope="col" role="columnheader" data-o-table-data-type="number">Name</th>';
+                table += '        <th data-column-width scope="col" role="columnheader" data-o-table-data-type="number"><a class="remove-user">Remove</a></th>';
+                table += '    </tr>';
+                table += '</thead>';
+                table += '<tbody>';
+                table += rows;
+                table += '</tbody>';
+                table += '</table>';
+                document.querySelector('.o-table-scroll-wrapper').innerHTML = table;
+            } else {
+                console.log('Request failed.  Returned status of ' + xhr.status);
+            }
+        };
+        xhr.send();
+    }
+
+    updateUserInfo();
 
 })();
