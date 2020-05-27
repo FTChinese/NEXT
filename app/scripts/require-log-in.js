33fa708042cf2fb963d498ceacb7a1c0a5509b7b
olizh <[oli.zhang@gmail.com]>
2020-05-27 15:53:31 +0800

    corona virus latest

--- app/scripts/require-log-in.js
+++ app/scripts/require-log-in.js
@@ -1,13 +1,13 @@
 // MARK: - Only subscribers can visit any page on chineseft.com
 (function(){
-	if (location.hostname === 'www.chineseft.com' && /^\/users\/findpassword|\/users\/register$/.test(location.pathname) === false) {
+	if (/chineseft\.com/.test(location.hostname) && /^\/users\/findpassword|\/users\/register$/.test(location.pathname) === false) {
 		var userId = GetCookie('USER_ID');
 		var paywall = GetCookie('paywall');
 		if (userId === null) {
 			setTimeout(function(){
 				showOverlay('overlay-login');
 				document.querySelector('.overlay-title').innerHTML = '订户专享网站';
-				document.querySelector('.register-find').style.display = 'none';
+				document.querySelector('.register-find').innerHTML = '<a href="https://www.ftacademy.cn/subscription.html">购买会员</a><span></span><a href="/users/findpassword">找回密码</a>';
 				document.querySelector('.wx-login').style.marginTop = '15px';
 				var overlayBG = document.querySelector('.overlay-bg');
 				overlayBG.className = 'overlay-bg-fixed';
@@ -17,7 +17,7 @@
 				showOverlay('overlay-login');
 				var userName = GetCookie('USER_NAME') || '';
 				document.querySelector('.overlay-title').innerHTML = '订户专享网站';
-				document.querySelector('.overlay-content form').innerHTML = '<div class="input-title">亲爱的用户您好，这个网站是付费订户专享，您当前使用的用户名' + userName + '并非用户。请登出之后更换用户名或者访问FT中文网主站。</div><div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" href="/users/logout">登出</a></div></div><div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" href="http://www.ftchi">免费网站</a></div></div>';
+				document.querySelector('.overlay-content form').innerHTML = '<div class="input-title">亲爱的用户您好，这个网站是付费订户专享，您当前使用的用户名' + userName + '并非用户。如果您是刚刚付款成功，请等待一分钟后刷新本页。否则请登出之后更换用户名或者访问FT中文网主站。</div><div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" href="/users/logout">登出</a></div></div><div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" href="http://www.ftchi">免费网站</a></div></div>';
 				var overlayBG = document.querySelector('.overlay-bg');
 				overlayBG.className = 'overlay-bg-fixed';
 			}, 0);
