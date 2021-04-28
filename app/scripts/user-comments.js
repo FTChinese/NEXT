0a75e9e27cbeab409f1bf8e787c9c2453621159a
Oliver Zhang <oli.zhang@gmail.comm>
2021-04-28 18:11:06 +0800

    update

--- app/scripts/user-comments.js
+++ app/scripts/user-comments.js
@@ -461,7 +461,10 @@ function checkLogin() {
     // MARK: - This only applies to iOS app
     var wechatFTCBindingEle = document.getElementById('wechat-ftc-binding');
     if (wechatFTCBindingEle && typeof window.userId === 'string' && window.userId.indexOf('ogfvw') === 0) {
-        wechatFTCBindingEle.innerHTML = '<a href="wechatftcbinding://' + window.userId + '" class="button ui-light-btn">绑定邮箱</a>';
+        href="http://www.ftchinese.com/m/corp/preview.html?pageid=subscriptioninfoconfirm&to=all" class="button ui-light-btn"
+        wechatFTCBindingEle.href = 'wechatftcbinding://' + window.userId;
+        wechatFTCBindingEle.className = 'button ui-light-btn';
+        wechatFTCBindingEle.innerHTML = '绑定邮箱';
     }
     passLoginToNative();
 }
