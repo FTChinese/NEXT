5d518d348da4f50fdd27cfbc5f2f3469200fcb5a
Oliver Zhang <oli.zhang@gmail.comm>
2021-05-26 11:21:27 +0800

    wechat binding

--- app/scripts/user-comments.js
+++ app/scripts/user-comments.js
@@ -461,9 +461,7 @@ function checkLogin() {
     // MARK: - This only applies to iOS app
     var wechatFTCBindingEle = document.getElementById('wechat-ftc-binding');
     if (wechatFTCBindingEle && typeof window.userId === 'string' && window.userId.indexOf('ogfvw') === 0) {
-        // wechatFTCBindingEle.href = 'wechatftcbinding://' + window.userId;
         wechatFTCBindingEle.className = 'button ui-light-btn wechat-binding';
-        wechatFTCBindingEle.onclick = window.weChatBinding;
         wechatFTCBindingEle.innerHTML = '绑定邮箱';
     }
     passLoginToNative();
