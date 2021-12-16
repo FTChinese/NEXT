06456f61c0547006851c9269e5d5e79ed75c72b4
Oliver Zhang <oli.zhang@gmail.comm>
2021-12-16 11:46:12 +0800

    overlay title for required login

--- app/scripts/require-log-in.js
+++ app/scripts/require-log-in.js
@@ -1,11 +1,12 @@
 // MARK: - Only subscribers can visit any page on chineseft.com
 (function(){
+	var overlayTitle = 'FT中文网付费会员专享网站';
 	function showPayWallForMember(userId) {
 		setTimeout(function(){
 			showOverlay('overlay-login');
 			var userName = GetCookie('USER_NAME') || '';
 			var phoneBindingHTML = (/^phone/.test(userId)) ? '<div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" id="phone-ftc-binding" onclick="showPhoneFTCBinding()">关联邮箱</a></div></div>' : '';
-			document.querySelector('.overlay-title').innerHTML = '订户专享网站';
+			document.querySelector('.overlay-title').innerHTML = overlayTitle;
 			document.querySelector('.overlay-content form').innerHTML = '<div class="input-title">亲爱的用户您好，这个网站是付费订户专享，您当前使用的用户名' + userName + '并非订户。如果您是刚刚付款成功，请等待一分钟后刷新本页。否则请登出之后更换用户名或者访问FT中文网主站。</div><div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" href="/users/logout">登出</a></div></div><div style="padding: 0 0 15px 0;"><div class="login-btn input-submit-container center"><a class="n-button-inner" href="http://www.ftchinese.com/">免费网站</a></div></div>' + phoneBindingHTML;
 			var overlayBG = document.querySelector('.overlay-bg');
 			overlayBG.className = 'overlay-bg-fixed';
@@ -23,7 +24,7 @@
 			// MARK: - The server side has a bug where sometimes cookie for user id might be empty but payment is not null. In that case, don't pop out the overlay. 
 			setTimeout(function(){
 				showOverlay('overlay-login');
-				document.querySelector('.overlay-title').innerHTML = '订户专享网站';
+				document.querySelector('.overlay-title').innerHTML = overlayTitle;
 				document.querySelector('.register-find').innerHTML = '<a href="https://www.ftacademy.cn/subscription.html">购买会员</a><span></span><a href="/users/findpassword">找回密码</a>';
 				document.querySelector('.wx-login').style.marginTop = '15px';
 				var overlayBG = document.querySelector('.overlay-bg');
