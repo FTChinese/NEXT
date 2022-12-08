bf4611229b1692246423c94456897054a502aa90
Oliver Zhang <oli.zhang@gmail.comm>
2022-12-08 11:59:16 +0800

    backup

--- app/scripts/inread.js
+++ app/scripts/inread.js
@@ -8,7 +8,11 @@ function initInreadAd() { //QUEST:这个也需要改成writeAdNew的方式吗？
     var inreadAdSlot = document.querySelectorAll(inreadAd.slot)[inreadAd.minSlot];
     var inreadAdContainer = document.createElement('div');
     var inreadAdWidth = inreadAdSlot.parentNode.offsetWidth;
-    var inreadAdHeight = Math.round(inreadAdWidth * 9 / 16);
+    var videoRatio = 9 / 16;
+    if (inreadAd.videoWidth > 0 && inreadAd.videoHeight > 0) {
+        videoRatio = inreadAd.videoHeight / inreadAd.videoWidth;
+    }
+    var inreadAdHeight = Math.round(inreadAdWidth * videoRatio);
     var hostForVideo = '';
     var s = inreadAdContainer.style;
     var clickTag = '';
@@ -17,16 +21,12 @@ function initInreadAd() { //QUEST:这个也需要改成writeAdNew的方式吗？
     var isHTML5Video = (typeof(document.createElement('video').canPlayType) !== 'undefined');
     var videoTag;
     var muted = (inreadAd.sound === 'on') ? '' : ' muted'; 
-
-
     if (inreadAd.forceWidth && inreadAd.forceWidth > 0) {
         inreadAdWidth = inreadAd.forceWidth;
     }
-
     if (inreadAd.forceHeight && inreadAd.forceHeight > 0) {
         inreadAdHeight = inreadAd.forceHeight;
     }
-
     inreadAd.h = inreadAdHeight + 30;
     if (window.location.hostname === 'localhost' || window.location.hostname.indexOf('192.168') === 0 || window.location.hostname.indexOf('10.113') === 0 || window.location.hostname.indexOf('127.0') === 0) {
         hostForVideo = 'https://www.ftchinese.com';
@@ -50,14 +50,14 @@ function initInreadAd() { //QUEST:这个也需要改成writeAdNew的方式吗？
     // MARK: - If HTML 5 video is supported and the video url is not empty;
     if (isHTML5Video && inreadAd.video !== '') {
         videoTag = '<video style="width:'+ inreadAdWidth +'px;height:'+ inreadAdHeight +'px;" preload="metadata" poster="' + inreadAd.poster + '"' + muted +' autoplay loop><source src="'+ inreadAd.video +'" type="video/mp4"></video>';
-        inreadAd.htmlCode = '<div style="width:100%;height:'+ inreadAdHeight +'px;position:relative;background:black;">' + videoTag + '</div>' + clickTag;
+        inreadAd.htmlCode = '<div style="width:100%;height:'+ inreadAdHeight +'px;position:relative;">' + videoTag + '</div>' + clickTag;
     } else {
         if (inreadAd.iframeSrc && inreadAd.iframeSrc !== '') {
             iframeSrc =  inreadAd.iframeSrc;
         } else {
             iframeSrc =  hostForVideo + '/m/corp/video.html?vid='+ inreadAd.vid +'&w='+inreadAdWidth+'&h='+inreadAdHeight+'&autostart=true&sound=' + inreadAd.sound +'&003';
         }
-        inreadAd.htmlCode = '<div style="width:100%;height:'+ inreadAdHeight +'px;position:relative;background:black;"><iframe name="inread-frame" id="inread-frame" style="width:'+ inreadAdWidth +'px;height:'+ inreadAdHeight +'px;position:absolute;" src="' + iframeSrc + '" scrolling="no" frameborder="0" allowfullscreen=true></iframe></div>' + clickTag;
+        inreadAd.htmlCode = '<div style="width:100%;height:'+ inreadAdHeight +'px;position:relative;"><iframe name="inread-frame" id="inread-frame" style="width:'+ inreadAdWidth +'px;height:'+ inreadAdHeight +'px;position:absolute;" src="' + iframeSrc + '" scrolling="no" frameborder="0" allowfullscreen=true></iframe></div>' + clickTag;
     }
     inreadAdSlot.parentNode.insertBefore(inreadAdContainer, inreadAdSlot);
     inreadAd.t = findTop(inreadAdContainer);
