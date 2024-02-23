4357e6f9e74d5a1e92dbe98c540e198a89a9f853
Oliver Zhang <oli.zhang@gmail.comm>
2024-02-23 13:37:25 +0800

    ad

---
+++ app/scripts/ad-fix-old-versions.js
@@ -0,0 +1,38 @@
+// MARK: - Google upgraded the gpt.js with breaking change and doesn't allow old versions to work. Use this to reverse-engineer the page to reload all ads. 
+(function() {
+    function loadScript(url, callback) {
+        var timeStamp = window.timeStamp || 'v001';
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+    
+        // If the script loads successfully, execute the callback
+        script.onload = function() {
+            if (callback) callback();
+        };
+    
+        // Handle errors in loading the script
+        script.onerror = function() {
+            console.error("Script load failed: " + url);
+        };
+        
+        script.src = url + '?' + timeStamp;
+        document.head.appendChild(script);
+    }
+    
+    // Function to load the first script and then the second
+    function loadScriptsSequentially() {
+        loadScript("https://d2785ji6wtdqx8.cloudfront.net/js/gpt.js", function() {
+            console.log("First script loaded successfully.");
+            window.adCodeLoaded = false;
+            // MARK: - This is a key step to destroy Google's blockage on ad display
+            window.googletag = undefined;
+            loadScript("https://d2785ji6wtdqx8.cloudfront.net/n/ad-polyfill.js", function() {
+                console.log("Second script loaded and run successfully.");
+            });
+        });
+    }
+    
+    // Load the scripts when the window loads
+    loadScriptsSequentially();
+})();
+
