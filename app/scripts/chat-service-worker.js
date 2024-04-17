304c6eec368b099058b0dedb907dc5b8311ae85d
Oliver Zhang <oli.zhang@gmail.comm>
2024-04-17 10:21:26 +0800

    style for chat

--- app/scripts/chat-service-worker.js
+++ app/scripts/chat-service-worker.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 
 
-const cacheName = 'v1094';
+const cacheName = 'v1095';
 console.log(`CACHE_NAME: ${cacheName}`);
 const domain = 'https://ftcoffer.herokuapp.com';
 const startUrl = '/powertranslate/chat.html';
