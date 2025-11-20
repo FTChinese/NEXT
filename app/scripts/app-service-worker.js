631f525e82d0098ec9ef3d0bd91aa4918ebdc810
Oliver Zhang <oli.zhang@gmail.comm>
2025-11-20 15:15:26 +0800

    backup

--- app/scripts/app-service-worker.js
+++ app/scripts/app-service-worker.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 11 */
 /* global self, caches, fetch, Response */
 
-const cacheName = 'v81';
+const cacheName = 'v82';
 const LOG_PREFIX = '[SW ' + cacheName + ']';
 const ENABLE_SW_LOGS = false;
 if (ENABLE_SW_LOGS) {
