2e522db2f08f36b9b9bef7d274151eedad843fa0
Oliver Zhang <oli.zhang@gmail.comm>
2026-08-19 17:42:52 +0800

    promo box update

--- app/scripts/chat-service-worker.js
+++ app/scripts/chat-service-worker.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 
 
-const cacheName = 'v2996';
+const cacheName = 'v2998';
 console.log(`CACHE_NAME: ${cacheName}`);
 const domain = 'https://ftcoffer.herokuapp.com';
 const startUrl = '/powertranslate/chat.html';
